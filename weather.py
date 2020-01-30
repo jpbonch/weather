3d26ee9b091dfc39377a1c174be1ae1079f6fad7
tricknac <jp.bonchristiano@bol.com.br>
2020-01-30 22:34:18 +0000

    clean css

--- weather.py
+++ weather.py
@@ -1,33 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pyowm import OWM
 from datetime import datetime
 
-
 app = Flask(__name__)
 
-API_key = 'fb58b1ad50f20581ddeb9b35254bf1b4'
-owm = OWM(API_key)
+owm_API_key = 'fb58b1ad50f20581ddeb9b35254bf1b4'
+owm = OWM(owm_API_key)
 
-@app.route('/')
+def generate_weather(city_name):
+                obs = owm.weather_at_place(city_name)
+                w = obs.get_weather()
+
+                time = datetime.now().strftime("%A, %H:%M")
+
+                icon = w.get_weather_icon_url()
+
+                temp = str(int(w.get_temperature(unit='celsius')['temp']))
+                humid = 'humidity: ' + str(int(w.get_humidity())) + '%'
+                wind = 'wind speed: ' + str(int(float(w.get_wind()['speed']) * 3.6)) + 'km/h'
+                status = w.get_detailed_status().capitalize()
+
+                weather_data = [city_name, time, temp, humid, wind, status, icon]
+                return weather_data
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    error = ''
+    weather = generate_weather('London')
+    if request.method == 'POST':
+        city = request.form.get('city', 'London')
+        try:
+            weather = generate_weather(city)
+        except:
+            error = 'City not found'
 
-    place = 'London'
-    obs = owm.weather_at_place(place)
-    w = obs.get_weather()
 
-    time = datetime.now().strftime("%A, %H:%M")
-
-    icon = w.get_weather_icon_url()
-
-    temp = str(int(w.get_temperature(unit='celsius')['temp'])) + '°C'
-    humid = 'humidity: ' + str(w.get_humidity()) + '%'
-    wind = 'wind speed: ' + str(float(w.get_wind()['speed']) * 3.6) + 'km/h'
-    status = w.get_detailed_status()
-
-    weather_data = []
-    weather_data.extend([place, time, temp, humid, wind, status, icon])
-
-    return render_template('index.html', weather_data=weather_data)
+    return render_template('index.html', weather_data=weather, error=error)
 
 if __name__ == '__main__':
 	app.run()
